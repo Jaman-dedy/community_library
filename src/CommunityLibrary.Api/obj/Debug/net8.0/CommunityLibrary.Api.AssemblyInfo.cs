af93d794b8dc5105d4adae367de208de240a40d7
Jaman-dedy <jeandedieuam@gmail.com>
2024-07-31 10:58:18 +0200

    setup project dependencies

--- src/CommunityLibrary.Api/obj/Debug/net8.0/CommunityLibrary.Api.AssemblyInfo.cs
+++ src/CommunityLibrary.Api/obj/Debug/net8.0/CommunityLibrary.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommunityLibrary.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abffb5b6217a9fedee8668097befbd2ae42980a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommunityLibrary.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommunityLibrary.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/CommunityLibrary.Infrastructure/obj/Debug/net8.0/CommunityLibrary.Infrastructure.AssemblyInfo.cs
+++ src/CommunityLibrary.Infrastructure/obj/Debug/net8.0/CommunityLibrary.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommunityLibrary.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abffb5b6217a9fedee8668097befbd2ae42980a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommunityLibrary.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommunityLibrary.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
